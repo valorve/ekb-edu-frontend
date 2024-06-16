7d311b41b2218c6d0cd1a6a4a738ebf7b91f1a67
valorve <valorve@protonmail.com>
2024-06-16 22:51:44 +0500

    fix pizdec

--- pages/index.vue
+++ pages/index.vue
@@ -1,50 +1,23 @@
 <template>
   <div class="content">
-    <h1>
-      Ваш путь к успеху начинается с EKB EDU!
-    </h1>
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc in nunc id lorem lacinia convallis id a felis. Sed mollis, risus eget ultricies dignissim, magna eros commodo mi, quis aliquam massa mauris eu libero. Donec vel sodales dolor, eget auctor augue. Donec enim mi, euismod sit amet posuere vitae, tempor vel tortor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur tempus, massa sed pellentesque pellentesque, arcu tellus cursus justo, vel ultricies diam lacus vel orci. Phasellus vitae sem id tortor tristique vulputate.
 
-    <p>
+  Donec vitae eros vel neque cursus condimentum. Aliquam pulvinar risus ac augue pharetra lobortis. Praesent a porta augue, quis scelerisque diam. Nulla ut rutrum diam. Nulla venenatis sapien id rutrum commodo. Nulla vitae tortor vel nibh consequat posuere at id odio. Proin ullamcorper volutpat consequat. Sed dapibus fermentum justo, et accumsan nisi. Nam ante neque, viverra et convallis sit amet, pellentesque at tellus. Mauris ac posuere lorem, ac maximus ligula. Donec finibus lobortis ex quis ultricies. Praesent sit amet velit fringilla, cursus dolor a, ullamcorper felis. Quisque ligula tellus, interdum vitae sagittis non, dapibus eu nibh. Aenean quis ante id nisl eleifend lobortis a elementum odio.
 
-      В мире, где знания - это сила, EKB EDU предлагает вам ключ к неограниченным возможностям обучения. Наша платформа дистанционного образования создана, чтобы вдохновлять, обучать и помогать вам расти, не выходя из дома. С курсами, охватывающими все, от технологий до творчества, мы предоставляем инструменты и знания, необходимые для достижения ваших амбиций.
-    </p>
+  Donec nec gravida mi, at mollis libero. In vel pretium nisl. Sed nisi leo, pulvinar vitae blandit vel, suscipit cursus dui. Duis varius sapien id volutpat pretium. Donec rhoncus dui eros, id congue nisl mollis et. Vivamus dignissim neque et tellus tempor fringilla. Duis faucibus quis ex et fermentum. Nulla bibendum felis ligula, commodo aliquam ligula dignissim quis.
 
-    Что мы предлагаем:
+  Mauris mattis lacinia massa eget condimentum. Aenean ut erat quis elit auctor lacinia congue rutrum purus. Proin iaculis, lectus id maximus maximus, enim enim gravida mauris, quis mattis mauris purus ac ligula. Integer tincidunt odio dolor, viverra molestie diam mollis non. Cras purus libero, fringilla nec purus vel, tincidunt ultricies nibh. Cras fringilla tellus vel varius rhoncus. Pellentesque convallis augue sed pulvinar venenatis. Pellentesque at sapien nec nulla congue fermentum ac eget tellus.
 
-    <ul>
-      <li>
-        Широкий спектр курсов: Изучайте новые области или углубляйтесь в уже известные темы с нашим разнообразием курсов. От программирования до дизайна, от маркетинга до управления проектами - у нас есть все, что нужно для вашего профессионального и личного роста.
-      </li>
+  Nam aliquam, felis eget venenatis maximus, diam ex laoreet est, in lacinia nibh enim et felis. Vivamus congue massa et aliquet fermentum. Vestibulum tincidunt accumsan augue, ac blandit quam facilisis id. Pellentesque at diam consequat, tempus risus sed, tristique nibh. Suspendisse commodo justo at nisi tincidunt, ut porta mi scelerisque. Maecenas scelerisque ullamcorper ipsum facilisis lobortis. In dapibus suscipit mauris, vitae sollicitudin risus mattis vel. Fusce cursus tempor elit, ut consequat arcu dapibus eu. Etiam ut faucibus felis. Cras at pulvinar sapien, a elementum dolor. Etiam et arcu eu erat rutrum fermentum ac non est. Vestibulum sollicitudin sem eget consequat maximus. Duis et turpis nec ligula fermentum maximus viverra vitae ante. Nam iaculis sagittis justo, sed accumsan ante laoreet sed. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur suscipit tellus quis quam iaculis ornare. 
+  <h3>
+    <NuxtLink to="/courses">
+      Перейти к курсам
+    </NuxtLink>
+  </h3>
 
-      <li>
-        Гибкость и удобство: Наши курсы разработаны так, чтобы удовлетворить потребности занятых людей. Учитесь в своем собственном темпе, в любое время и в любом месте.
-      </li>
-
-      <li>
-        Поддержка сообщества: Присоединяйтесь к сообществу единомышленников и профессионалов. Обменивайтесь идеями, задавайте вопросы и получайте поддержку от своих товарищей и наставников.
-      </li>
-
-      <li>
-        Практический опыт: Наши курсы не только дают теоретические знания, но и предлагают практические задания, проекты и кейсы, чтобы вы могли применять изученное на практике.
-      </li>
-    </ul>
-     
-    <p>
-      Почему выбирают нас?
-    </p>
-    <p>
-      Мы верим, что образование должно быть доступным, эффективным и интересным. Мы стремимся предоставлять качественные образовательные ресурсы, чтобы вы могли достигать своих целей быстрее и увереннее.
-    </p>
-
-    <h3>
-      <NuxtLink to="/courses">
-        Перейти к курсам
-      </NuxtLink>
-    </h3>
-
-    <p>
-      Ознакомьтесь с полным каталогом курсов и начните обучение уже сегодня.
-    </p>
+  <p>
+    Ознакомьтесь с полным каталогом курсов и начните обучение уже сегодня.
+  </p>
   </div>
 </template>
 
